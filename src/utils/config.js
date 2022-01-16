ddd3b798911c16afb9d15295ff845d194d631c22
Luka Rosić <37700386+lrosic00@users.noreply.github.com>
2022-01-16 17:56:47 +0100

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 //Get from console.cloud.google.com under APIs & Services/Credentials
 export const CLIENT_ID =
-	"695537195445-9i7lrlrav1m703e33h0s809b9fltt27v.apps.googleusercontent.com";
+	"695537195445-ptoelogi1fjma09l303jb6o3cq23vuv3.apps.googleusercontent.com";
 export const API_KEY = "AIzaSyDz5-33SwUgZdQQngIVsup582laeJ150JE";
 
 export const DISCOVERY_DOCS = [
